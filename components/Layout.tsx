b80b9d5fb91a50aeb167b5a13b83111a395581ae
Julu1994 <mrjewel837@gmail.com>
2023-06-25 00:58:06 +0200

    completed the home page

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,32 +1,36 @@
 import React, { ReactNode } from 'react'
 import Link from 'next/link'
 import Head from 'next/head'
+import SearchAppBar from './appbar'
+import { CssBaseline } from '@mui/material'
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
 
 type Props = {
   children?: ReactNode
   title?: string
 }
+const theme = createTheme({
+  typography: {
+    fontFamily: 'PT Serif, serif',
+  },
+});
 
 const Layout = ({ children, title = 'This is the default title' }: Props) => (
-  <div>
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
     <Head>
       <title>{title}</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <link href="https://fonts.googleapis.com/css2?family=PT+Serif&display=swap" rel="stylesheet" />
     </Head>
     <header>
-      <nav>
-        <Link href="/">Home</Link> | <Link href="/about">About</Link> |{' '}
-        <Link href="/users">Users List</Link> |{' '}
-        <a href="/api/users">Users API</a>
-      </nav>
+      <SearchAppBar />
     </header>
     {children}
-    <footer>
-      <hr />
-      <span>I'm here to stay (Footer)</span>
-    </footer>
-  </div>
-)
+  </ThemeProvider>
+);
+
 
 export default Layout
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,106 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import styled from '@emotion/styled';
+import { Avatar, Box, Divider, List, ListItem, ListItemAvatar, ListItemText, Typography } from '@mui/material';
+import { GetStaticProps } from 'next';
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+import Layout from '../components/Layout';
+type User = {
+  company: any;
+  id: number;
+  name: string;
+};
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">About</Link>
-    </p>
-  </Layout>
-)
+type HomePageProps = {
+  users: User[];
+};
+const BackgroundBox = styled(Box)(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center', 
+  justifyContent: 'center', 
+  position: 'relative',
+  height: '100%',
+  '&::before': {
+    content: '""',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+    backgroundImage: `url('./cover.png')`,
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+    zIndex: -1
+  },
+  '&::after': {
+    content: '""',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+    backgroundColor: 'rgba(173, 216, 230, 0.5)',
+    zIndex: -1
+  },
+}));
 
-export default IndexPage
+const Content = styled('div')({
+  position: 'relative',
+  padding: '1rem',
+  zIndex: 1,
+  extAlign: 'center'
+});
+
+export default function HomePage({ users }: HomePageProps) {
+  return (
+    <Layout>
+      <BackgroundBox>
+        <Content>
+          <Typography variant="h3" component="h2"
+            sx={{ color: "	#FFFFFF", fontWeight: "900", textShadow: "2px 2px 4px rgba(0, 0, 0, 0.5)", textAlign: "center", mb: "2rem", mt: "2rem" }}>
+            User List
+          </Typography>
+          <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+            {users.map(user => (
+              <React.Fragment key={user.id}>
+                <ListItem alignItems="flex-start">
+                  <ListItemAvatar>
+                    <Avatar alt={user.name} src="/static/images/avatar/1.jpg" />
+                    </ListItemAvatar>
+                
+                    <ListItemText
+                      primary= { <Link href={`/users/${user.id}`} style={{textDecoration: "none"}}>{user.name}</Link>}
+                      secondary={
+                        <React.Fragment>
+                        <Typography
+                        sx={{ display: 'inline' }}
+                        component="span"
+                        variant="body2"
+                        color="text.primary"
+                        >
+              {user.company.name}
+              </Typography>
+            </React.Fragment>
+          }>
+                  
+                  </ListItemText>
+
+              </ListItem>
+                <Divider variant="inset" component="li" />
+                </React.Fragment>
+            ))}
+          </List>
+        </Content>
+      </BackgroundBox>
+    </Layout> 
+  );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const users: User[] = await res.json();
+
+  return {
+    props: { users },
+  };
+};
